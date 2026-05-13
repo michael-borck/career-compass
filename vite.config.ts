1c19c9f40d517480d77be6cfd2ed75ce2bdb38eb
Michael Borck <michael@borck.dev>
2026-05-13 17:33:47 +0800

    feat(vite): add vite config and entry html

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/renderer'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5180,
+    strictPort: true,
+  },
+});
